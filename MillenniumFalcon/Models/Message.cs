4e5dbc78c981551d3606786cae7bb3dea98f5025
Jean Roux <jroux@mobii.com>
2018-09-20 16:51:00 +0200

    initial commit

---
+++ MillenniumFalcon/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MillenniumFalcon.Models
+{
+    /// <summary>
+    /// Construct a message that will be used to post to the queue
+    /// </summary>
+    public class Message
+    {
+        private string _messageText { get; set; }
+        public string Value
+        {
+            get
+            {
+                return _messageText;
+            }
+            set
+            {
+                _messageText = value;
+            }
+        }
+
+        public Message(string messageText)
+        {
+            _messageText = messageText;
+        }
+    }
+}
---
+++ MillenniumFalcon/Sender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MillenniumFalcon.Models;
+using RabbitMQ.Client;
+
+namespace MillenniumFalcon
+{
+    class Program
+    {
+        #region Fields
+        private static readonly string _hostName = "localhost";
+        #endregion"localhost"
+
+        /// <summary>
+        /// Entry point to the sender application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            SendHelper sendHelper = new SendHelper(factory);
+            Message message = UserPrompts();
+            sendHelper.SendToQueue(message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Setup UI to accept user input and validate.
+        /// </summary>
+        /// <returns>
+        /// <c>Message</c>
+        /// <see cref="Message"/>
+        /// a message object containing the required content to pass to the queue
+        /// </returns>
+        public static Message UserPrompts()
+        {
+            string name = "";
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Please Enter your name: ");
+                name = Console.ReadLine().Trim();
+            } while (name == "");
+            
+
+            Message message = new Message(name);
+
+            return message;
+        }
+    }
+}
